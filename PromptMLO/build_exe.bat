1eab61307a99fcc5a36c2a18ce5e88bf82937d07
Dominic Epting <92177107+domtheecom@users.noreply.github.com>
2025-12-22 00:10:57 -0500

    Delete PromptMLO directory

--- PromptMLO/build_exe.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-python -m pip install --upgrade pyinstaller
-pyinstaller --onefile --noconsole --name PromptMLO_Studio app.py
-
-echo Build complete. EXE is in the dist folder.
-endlocal
